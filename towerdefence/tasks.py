e45ee28722f068d990ec9ef17418b421b85bd0f2
kuussant <santeri.kuusisto@helsinki.fi>
2024-11-19 22:56:09 +0200

    fixed tasks.py

--- towerdefence/tasks.py
+++ towerdefence/tasks.py
@@ -1,4 +1,6 @@
 from invoke import task
+from subprocess import call
+from sys import platform
 
 @task
 def start(ctx):
@@ -9,5 +11,7 @@ def test(ctx):
     ctx.run("poetry run pytest", pty=True)
 
 @task
-def coverage(ctx):
+def coverage_report(ctx):
     ctx.run("coverage run --branch -m pytest; coverage html", pty=True)
+    if platform != "win32":
+        call(("xdg-open", "htmlcov/index.html"))
